841a6d04769b13e5397614e83403d761f2479b3d
Darryl <darrylmangibin@gmail.com>
2019-07-15 02:54:38 +0800

    UPDATE - styles

--- client/src/components/Notes/index.js
+++ client/src/components/Notes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Notes.module.css';
+import { Link } from 'react-router-dom';
 
 import Button from '../../common/button'
 
@@ -7,10 +8,10 @@ const Notes = () => {
   return (
     <div className="container">
       <p className={`my-5 text-center`}>No notes to show</p>
-      <a className={classes.note}>
+      <Link to="/11" className={classes.note}>
         <p className={classes.title}>Unnamed note</p>
         <p className={classes.subtitle}>Last edited a few seconds ago</p>
-      </a>
+      </Link>
       <Button 
         title="Create Note"
         background="#43799c"
