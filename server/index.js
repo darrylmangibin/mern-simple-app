ab2a41b31c7fe41e27e20b16bc1f9ca9d229d36a
Darryl <darrylmangibin@gmail.com>
2019-07-14 21:47:42 +0800

    middleware and datase configuration

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const mongoose = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv').config();
 
 const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// MongoDB configuration;
+const db = process.env.DATABASE;
+mongoose.connect(db, { useNewUrlParser: true }).then(() => {
+  console.log('MongoDB connected')
+}).catch((err) => {
+  console.log(err)
+})
 
 // PORT
 const port = process.env.PORT || 5000;
